0962346701848535690d63f1be853b3144f7a4bf
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-06-28 09:41:08 -0500

    Update HelloRestController.java

--- HelloSpringBoot/src/main/java/com/makotojava/learn/HelloSpringBoot/HelloRestController.java
+++ HelloSpringBoot/src/main/java/com/makotojava/learn/HelloSpringBoot/HelloRestController.java
@@ -1,7 +1,8 @@
 package com.makotojava.learn.HelloSpringBoot;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*")
 	@RestController
 	public class HelloRestController {
 		@RequestMapping("/hola")
